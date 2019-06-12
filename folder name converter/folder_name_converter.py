42f97f437394171be51a25ac9d282c5e66d08d75
Guo Zhengjin <guo.zhengjin@gmail.com>
2019-06-12 23:34:44 +0800

    data need to be cleaned first before feeded to processing

--- folder name converter/folder_name_converter.py
+++ folder name converter/folder_name_converter.py
@@ -1,6 +1,7 @@
 from pypinyin import lazy_pinyin
 import os
 import urllib
+import csv
 
 path = r'D:\Gallery\Chinese Temple'
 
@@ -9,3 +10,44 @@ filelist = os.listdir(path)
 file1 = filelist[0]
 print(file1)
 print(''.join(lazy_pinyin(file1)))
+
+temple_ids = {}
+
+with open('folder name converter/chinese_temple_list.csv', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    chinese_temple_list = list(csv_reader)
+
+    for temple in chinese_temple_list:
+        # # check duplicated temple names
+        # if temple[4] in temple_ids:
+        #     print(temple[4])
+
+        temple_ids[temple[4]] = temple[0]
+
+# print(temple_ids)
+# print(len(temple_ids))
+
+notFoundCount = 0
+for file in filelist:
+    # print(file)
+    chunks = file.split('_')
+    _chunks = []
+    found = False
+    
+
+    for chunk in chunks:
+        if chunk in temple_ids:
+            # print('xxxx', chunk, temple_ids[chunk])
+            _chunks.append(temple_ids[chunk])
+            found = True
+        else:
+            _chunks.append(chunk)
+        
+    if not found:
+        notFoundCount += 1
+        print(notFoundCount, chunks)
+        
+
+    # print(_chunks)
+    new_file_name = '_'.join(_chunks)
+    # print(new_file_name)
